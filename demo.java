3d55dd7257b209a04db35d62243ab2b25c3b00b1
sampoo123 <biswas1907095@stud.kuet.ac.bd>
2023-07-08 02:26:46 -0700

    push

---
+++ demo.java
@@ -0,0 +1,16 @@
+public demo{
+    int a,b;
+    public demo()
+    {
+
+    }
+    public demo(int a,int b){
+        this.a=a;
+        this.b=b;
+    }
+
+    public int getA(){return a;}
+    public int getB(){ return b;}
+    public void setS(int a){this.a=a;}
+    public void setB(int b){this.b=b;}
+}
